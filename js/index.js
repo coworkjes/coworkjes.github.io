74f6494926267071e8128cdd2459a0f093ad8c8b
dala <dalibor.janak@gmail.com>
2025-01-19 22:29:41 +0100

    Remove google map

--- js/index.js
+++ js/index.js
@@ -1,14 +1,18 @@
 function initMap() {
-  var myLatLng = {lat: 50.230121, lng: 17.204888};
+  let center = [17.204888, 50.230121];
+  maptilersdk.config.apiKey = 'G2BcJ3X6QCNAYylpOlNW';
+  let style = '14b57dde-0f1d-4755-a8f5-e85b7f9e2fc6';
 
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 16,
-    center: myLatLng
+  const map = new maptilersdk.Map({
+    container: 'map', // container's id or the HTML element to render the map
+    style: style,
+    center: center, 
+    zoom: 12,
   });
 
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'CoworkJes!'
-  });
+  const marker = new maptilersdk.Marker({
+    color: '#1683A6'
+  })
+  .setLngLat(center)
+  .addTo(map);
 }
